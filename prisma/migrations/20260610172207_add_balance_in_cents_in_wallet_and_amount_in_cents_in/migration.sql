1d678db529a429e2a4ab361890f4f8ff6ecb98c3
Roma4286 <lionkrou8@gmail.com>
2026-07-02 22:56:52 +0300

    refactoring(migration): transferred datа-migration to sql

--- prisma/migrations/20260610172207_add_balance_in_cents_in_wallet_and_amount_in_cents_in/migration.sql
+++ prisma/migrations/20260610172207_add_balance_in_cents_in_wallet_and_amount_in_cents_in/migration.sql
@@ -3,3 +3,13 @@ ALTER TABLE "Transaction" ADD COLUMN     "amountInCents" INTEGER;
 
 -- AlterTable
 ALTER TABLE "Wallet" ADD COLUMN     "balanceInCents" INTEGER NOT NULL DEFAULT 0;
+
+-- DataMigration
+UPDATE "Wallet"
+SET "balanceInCents" = ROUND("balance" * 100)::INTEGER;
+
+UPDATE "Transaction" AS t
+SET "amountInCents" = ROUND(t."amount" * 100)::INTEGER
+  * CASE WHEN c."type" = 'EXPENSE' THEN -1 ELSE 1 END
+FROM "Category" AS c
+WHERE t."categoryId" = c."id";
